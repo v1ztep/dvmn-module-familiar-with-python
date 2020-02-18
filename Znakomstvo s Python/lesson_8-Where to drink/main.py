b0cbf6c7355321874ce6bd90e8382f00df6109c4
v1ztep <v1ztep@gmail.com>
2020-02-18 15:59:36 +0300

    Изменил имена переменных

--- Znakomstvo s Python/lesson_8-Where to drink/main.py
+++ Znakomstvo s Python/lesson_8-Where to drink/main.py
@@ -17,50 +17,50 @@ def fetch_coordinates(APIKEY, place):
     return lon, lat
 
 
-def get_bar_distance(bars_around):
-    return bars_around['distance']
+def get_bar_distance(bar_around):
+    return bar_around['distance']
 
 def main():
     load_dotenv()
     APIKEY = os.getenv('YANDEX_GEOCODER_APIKEY')
 
-    my_coordinates = fetch_coordinates(APIKEY, input('Где вы находитесь? '))
+    user_coordinates = fetch_coordinates(APIKEY, input('Введите своё местоположение: '))
 
     bars_around = []
 
     with open('bars.json', 'r', encoding='CP1251') as my_file:
-        file_contents = json.loads(my_file.read())
+        bars_contents = json.loads(my_file.read())
 
-        for bar_info in file_contents:
+        for bar_info in bars_contents:
             bar_around_info = {
-                'distance': distance.distance((my_coordinates[1], my_coordinates[0]), (bar_info['geoData']['coordinates'][1], bar_info['geoData']['coordinates'][0])).km,
+                'distance': distance.distance((user_coordinates[1], user_coordinates[0]), (bar_info['geoData']['coordinates'][1], bar_info['geoData']['coordinates'][0])).km,
                 'latitude': bar_info['geoData']['coordinates'][1],
                 'longitude': bar_info['geoData']['coordinates'][0],
                 'title': bar_info['Name']
             }
             bars_around.append(bar_around_info)
 
-    sorted_five_bars = sorted(bars_around, key=get_bar_distance)[:5]
+    sorted_bars = sorted(bars_around, key=get_bar_distance)[:5]
 
-    me_and_five_bars_on_map = folium.Map(
-        location=[my_coordinates[1], my_coordinates[0]],
+    user_and_bars_on_map = folium.Map(
+        location=[user_coordinates[1], user_coordinates[0]],
         zoom_start=15
     )
 
     folium.Marker(
-        location=[my_coordinates[1], my_coordinates[0]],
-        popup='Me',
+        location=[user_coordinates[1], user_coordinates[0]],
+        popup='User',
         icon=folium.Icon(color='red', icon='info-sign')
-    ).add_to(me_and_five_bars_on_map)
+    ).add_to(user_and_bars_on_map)
 
-    for bar in sorted_five_bars:
+    for bar in sorted_bars:
         folium.Marker(
             location=[bar['latitude'], bar['longitude']],
             popup=bar['title'],
             icon=folium.Icon(color='green')
-        ).add_to(me_and_five_bars_on_map)
+        ).add_to(user_and_bars_on_map)
 
-    me_and_five_bars_on_map.save('index.html')
+    user_and_bars_on_map.save('index.html')
 
 
 
